05b30497b3a606fc624b8ce7a0e0946b36e51317
mwidling <mateus.wilding@codeminer42.com>
2023-07-24 09:15:20 -0300

    adds useEffect hook implt

--- src/Count.js
+++ src/Count.js
@@ -3,6 +3,10 @@ import { useSelector } from '../lib/react-redux'
 
 export function Count(){
     const counter = useSelector((state) => state.value)
+
+    React.useEffect(() => {
+        console.log("Called")
+    }, [])
   
     return <h1>{counter}</h1>
 }
